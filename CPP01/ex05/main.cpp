ef3e884113c09c409d21c58ab743b5bb2b5c1d77
rafaelabdm <rafaelabdm@hotmail.com>
2023-04-27 12:45:29 -0300

    style add void as main argument

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,13 +6,13 @@
 /*   By: rabustam <rabustam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 14:44:37 by rabustam          #+#    #+#             */
-/*   Updated: 2023/04/18 10:32:36 by rabustam         ###   ########.fr       */
+/*   Updated: 2023/04/27 12:44:06 by rabustam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main()
+int	main(void)
 {
 	Harl harl;
 	harl.complain("DEBUG");
